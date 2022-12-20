77bc61bce5d0658e9a8a79b44b4137710d9d6690
Washington Antunes <washington.cead@gmail.com>
2022-12-20 16:12:55 -0300

    Implementação da classe AbstractEquipment

---
+++ src/main/java/br/com/wti/erp/domain/AbstractEquipment.java
@@ -0,0 +1,74 @@
+package br.com.wti.erp.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import br.com.wti.erp.domain.enums.StatusComputerEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class AbstractEquipment extends AbstractEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@NotEmpty
+	@Column(name = "serial_number", unique = true, nullable = false, length = 30)
+	private String serialNumber;
+	
+	@NotEmpty
+	@Column(nullable = false, length = 10)
+	private String brand;
+
+	@NotEmpty
+	@Column(nullable = false, length = 30)
+	private String model;
+
+	@NotNull
+	@Column(name = "date_entry", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateEntry;
+
+	@NotEmpty
+	@Column(name = "note_entry", nullable = false, length = 20)
+	private String noteEntry;
+	
+	@NotEmpty
+	@Column(nullable = false, length = 30)
+	private String location;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 30)
+	private StatusComputerEnum status;
+
+	private String note;
+
+	@Column(precision = 10, scale = 2)
+	private BigDecimal value;
+	
+	@ManyToOne(optional = true)
+	@JoinColumn(name = "project_id", referencedColumnName = "id")
+	private Project project;
+
+	@OneToOne(optional = true)
+	@JoinColumn(name = "user_id", unique = true)
+	private User user;
+	
+}
